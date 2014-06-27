b2e0e59ad1d52b5661c63e0deb24846ebd55bf55
Michael J. Gilliland <mjg.py3@gmail.com>
2014-06-26 20:01:15 -0400

    Started TDD-ing constructor

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -1,3 +1,8 @@
 class Configurator
 
+  def initialize(file_name)
+    config_path = File.expand_path("../config/#{file_name}", __FILE__)
+    YAML.load_file(config_path)
+  end
+
 end
--- spec/configurator_spec.rb
+++ spec/configurator_spec.rb
@@ -1,9 +1,41 @@
 require './lib/configurator.rb'
 
+def file_under_test
+  __FILE__.sub('_spec', '').sub('spec', 'lib')
+end
+
 describe Configurator do
+
   describe '#initialize' do
-    subject { Configurator.new('some_file_name') }
+    before(:each) do
+      allow(YAML).to receive(:load_file).and_return(double.as_null_object)
+    end
+
+    subject { Configurator.new(file_name) }
+    let(:file_name) { 'some_file_name' }
 
     it { is_expected.to be_an_instance_of(Configurator) }
+
+    it 'reads the passed filename from ../config/' do
+      expect(File).to receive(:expand_path).with('../config/' + file_name, anything)
+      subject
+    end
+
+    it 'expands the path based on a file in the directory one above the current' do
+      expect(File).to receive(:expand_path).with(anything, file_under_test)
+      subject
+    end
+
+    context 'when the config file exists' do
+      let(:config_path) { 'path/to/config' }
+      before(:each) do
+        allow(File).to receive(:expand_path).and_return(config_path)
+      end
+
+      it 'loads the config file using YAML' do
+        expect(YAML).to receive(:load_file).with(config_path)
+        subject
+      end
+    end
   end
 end
