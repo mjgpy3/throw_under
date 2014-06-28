211b2ec9d76ce7e98dd9b6a2a9555384bf9dbdb7
Michael J. Gilliland <mjg.py3@gmail.com>
2014-06-28 11:14:44 -0400

    New RabbitMq connection

--- lib/queue_binder.rb
+++ lib/queue_binder.rb
@@ -1,6 +1,7 @@
 class QueueBinder
 
   def initialize(configurator)
+    Bunny.new(configurator.rabbit_url)
   end
 
 end
--- spec/queue_binder_spec.rb
+++ spec/queue_binder_spec.rb
@@ -1,15 +1,29 @@
 require './lib/queue_binder.rb'
+require 'bunny'
 
 describe QueueBinder do
+
+  before(:each) do
+    allow(Bunny).to receive(:new)
+  end
 
   describe '#initialize' do
     subject { QueueBinder.new(configurator) }
 
     context 'when given a Configurator' do
-      let(:configurator) { double('Configurator') }
+      let(:configurator) { double('Configurator', rabbit_url: 'some_url...') }
 
       it { is_expected.to be_instance_of(QueueBinder) }
+
+      it 'creates a new connection using the Configurator\'s rabbit url' do
+        expect(Bunny).to receive(:new).with(configurator.rabbit_url)
+        subject
+      end
+    end
+
+    describe '#bind_queues' do
+      subject { QueueBinder.new(configurator).bind_queues }
+
     end
   end
-
 end
